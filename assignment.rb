93f8b3a452719b2b446f1a53ec5d4c44ee6a9e0b
Chakra Timilsina <v-chakra@nebham.com>
2024-05-29 21:26:32 +0545

    feat: add function for print fizz buzz

--- assignment.rb
+++ assignment.rb
@@ -14,3 +14,27 @@ end
 # Example usage:
 puts greatest_value(1, 2, 3)
 puts greatest_value(3, 2, 1)
+
+############ 2 ############
+
+# prints numbers from 1 to 100
+# for multiples of 3, prints "Fizz"
+# for multiples of 5, prints "Buzz"
+# for multiples of 3 and 5, prints "FizzBuzz"
+# @return [void]
+def print_fizz_buzz
+  (1..100).each do |i|
+    mod_3 = (i % 3).zero?
+    mod_5 = (i % 5).zero?
+
+    if mod_3 && mod_5
+      puts "FizzBuzz"
+    elsif mod_3
+      puts "Fizz"
+    elsif mod_5
+      puts "Buzz"
+    else
+      puts i
+    end
+  end
+end
